343c68f7d15613959b30cf4fe82b2d7eabceb73a
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-17 14:44:36 +0530

    Routers Added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
-// URL PATH
+
 const express = require("express");
-const { home, createUser } = require("../controllers/userControllers");
+const { home, createUser , getUsers , editUser ,deleteUser  } = require("../controllers/userControllers");
 const router = express.Router();
 
 router.get("/", home);
 router.post("/createUser", createUser);
-
+router.get("/getUsers",getUsers)
+router.put("/editUsers/:id",editUser)
+router.delete("/deleteUser/:id",deleteUser)
 module.exports = router;
