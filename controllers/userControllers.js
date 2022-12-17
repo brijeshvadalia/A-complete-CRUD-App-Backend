10c331fa4d0b781b362f8198abbd98a48bad3490
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-17 11:29:30 +0530

    Delete User Logic Added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -61,4 +61,19 @@ exports.editUser = async (req, res) => {
   }
 };
 
-exports.deleteUser = async (req, res) => {};
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id, req.body);
+    res.status(200).json({
+      success: true,
+      message: "User updated Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
